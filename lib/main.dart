2a1d5afada43c3c2ff8fc61f7aad9c2e1c8d84a4
WaiLeong <cutegtp12345@gmail.com>
2025-04-22 13:44:25 +0800

    overtime pay

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 void main() {
   runApp(MyApp());
 }
@@ -12,95 +13,79 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController loanAmountCtrl = TextEditingController();
-  TextEditingController netIncomeCtrl = TextEditingController();
-  TextEditingController loanInterestCtrl = TextEditingController();
-  int? selectedItem;
-  bool? isGuarantorChecked = false;
-  String? selectedCarType;
-  double totalRepayment = 0;
-  bool? eligible;
-  double totalInterest = 0;
-  String? loanApplicationResult;
+  final TextEditingController workedHourCtrl = TextEditingController();
+  String? workTime;
+  String? workHour;
+  double? totalPay;
+  List<String> totalPaid = [];
 
-  bool isInterestRateValid(){
-    double? interest = double.tryParse(loanInterestCtrl.text);
-    if(interest == null || selectedCarType == null){
-      return false;
+  void _calculateWorkingHour(){
+    double workhour = double.tryParse(workedHourCtrl.text)!;
+    double regularHourRate = 15 * workhour;
+    double overtimeHourRate = (15 * 1.5) * workhour;
+
+    if(workhour == null){
+      setState(() {
+        workHour = 'Please enter your worked hour!';
+        workhour = 0;
+      });
     }
-
-    if(selectedCarType == 'New'){
-      return interest >= 2.5 && interest <= 3.2;
-    }
-    else if(selectedCarType == 'Used'){
-      return interest >= 2.5 && interest <= 3.2;
-    }else{
-      return false;
-    }
-  }
-
-
-  void _calculateInterest(){
-    double loan = double.tryParse(loanAmountCtrl.text)!;
-    double interest = double.tryParse(loanInterestCtrl.text)!;
-    int loanPeriod = selectedItem!;
-
-    totalInterest = loan * (interest / 100) * loanPeriod;
 
     setState(() {
-      totalInterest.toString();
-    });
-  }
-
-  void _calculateRepayment(){
-    double? loan = double.tryParse(loanAmountCtrl.text);
-    double? netIncome = double.tryParse(netIncomeCtrl.text);
-    double? interest = double.tryParse(loanInterestCtrl.text);
-    int loanPeriod = selectedItem!;
-    bool? guarantor = isGuarantorChecked;
-
-    if (loan == null || netIncome == null || interest == null || loanPeriod == null || selectedCarType == null) {
-      setState(() {
-        loanApplicationResult = 'Please fill in all fields correctly.';
-        totalRepayment = 0;
-        eligible = false;
-      });
-      return;
-    }
-
-    if(!isInterestRateValid()){
-      setState(() {
-        loanApplicationResult = 'Invalid interest rate for selected car type!';
-        totalRepayment = 0;
-        eligible = false;
-      });
-      return;
-    }
-
-    _calculateInterest();
-
-    totalRepayment = (loan + totalInterest) / (loanPeriod * 12);
-
-    setState(() {
-      if(totalRepayment <= (0.3 * netIncome)){
-        eligible = true;
-        loanApplicationResult = 'Eligible, You passed your loan application.';
+      if(workhour <= 40){
+        workTime = 'You worked regularly';
+        regularHourRate = 15 * workhour;
+        totalPay = regularHourRate;
       }
-      else if(totalRepayment > (0.3 * netIncome) && guarantor == true){
-        eligible = true;
-        loanApplicationResult = 'Eligible(with guarantor), You passed your loan application.';
+      else if(workhour > 40){
+        workTime = 'You worked regularly';
+        overtimeHourRate = (15 * 1.5) * workhour;
+        totalPay = regularHourRate + overtimeHourRate;
       }
-      else if(totalRepayment > (0.3 * netIncome) && guarantor == false){
-        eligible = false;
-        loanApplicationResult = 'not Eligible, You failed the loan application.';
-      }else{
+      else{
         return;
       }
     });
 
     setState(() {
-      loanApplicationResult.toString();
+      totalPay.toString();
     });
+  }
+
+
+  Future<void> _saveTotalPaid() async {
+    _calculateWorkingHour();
+    final prefs = await SharedPreferences.getInstance();
+    String? encodeCart = jsonEncode(totalPaid);
+    await prefs.setString('Paid', encodeCart);
+    Text('Total Paid: $totalPaid');
+  }
+
+  Future<void> _loadTotalPaid() async{
+    _saveTotalPaid();
+    final prefs = await SharedPreferences.getInstance();
+    String? encodeCart = prefs.getString('Paid');
+    if(encodeCart != null){
+      List<dynamic> decodedList = jsonDecode(encodeCart);
+      totalPaid = List<String>.from(decodedList);
+      for(int i = 0; i < totalPaid.length; i++){
+        Text('Total Paid Loaded: ${totalPaid[i]}');
+
+      }
+    }
+  }
+
+  //Add total paid to cart
+  void _addTotalPaid(String paidList){
+    totalPaid.add(paidList);
+    _saveTotalPaid();
+  }
+
+  Future<void> _clearPaidList() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('Paid');
+    totalPaid.clear();
+    const Text('Total Paid cleared');
   }
 
   @override
@@ -108,112 +93,51 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.cyan,
-          title: const Text('Car Loan'),
+          title: const Text('Overtime Pay'),
+          backgroundColor: Colors.green,
         ),
-        body: Center(
+        body:  Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             spacing: 8,
             children: [
-              const Text('Loan Amount'),
+              const Text('Worked hour: '),
               TextField(
-                controller: loanAmountCtrl,
                 keyboardType: TextInputType.number,
+                controller: workedHourCtrl,
                 decoration: const InputDecoration(
                   prefix: Icon(Icons.search),
                   suffixIcon: Icon(Icons.clear),
-                  label: Text('Enter your loan amount'),
-                  filled: true,
+                  label: Text('Enter your worked hour.'),
+                  hintText: '',
+                  helperText: '',
                   border: OutlineInputBorder(),
                 ),
-              ),
-              const Text('Net Income'),
-              TextField(
-                controller: netIncomeCtrl,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  prefix: Icon(Icons.search),
-                  suffixIcon: Icon(Icons.clear),
-                  label: Text('Enter your net income'),
-                  filled: true,
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              DropdownButton<int>(
-                value: selectedItem,
-                hint: const Text('Select loan period(Year)'),
-                items: <int>[1, 2, 3, 4, 5].map((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  );
-                }).toList(),
-                onChanged: (int? newValue) {
-                  setState(() {
-                    selectedItem = newValue;
-                  });
-                },
-              ),
-
-              const Text('Interest Rate'),
-              TextField(
-                controller: loanInterestCtrl,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  prefix: Icon(Icons.search),
-                  suffixIcon: Icon(Icons.clear),
-                  label: Text('Enter your interest Rate'),
-                  filled: true,
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              CheckboxListTile(
-                title: const Text('I have a guarantor'),
-                value: isGuarantorChecked,
-                onChanged: (bool? newValue) {
-                  setState(() {
-                    isGuarantorChecked = newValue ?? false;
-                  });
-                },
-              ),
-
-              RadioListTile(
-                title: const Text('New'),
-                  value: 'New',
-                  groupValue: selectedCarType,
-                  onChanged: (newValue){
-                    setState(() {
-                      selectedCarType = newValue.toString();
-                    });
-                  },
-              ),
-
-              RadioListTile(
-                title: const Text('Used'),
-                value: 'Used',
-                groupValue: selectedCarType,
-                onChanged: (newValue){
-                  setState(() {
-                    selectedCarType = newValue.toString();
-                  });
-                },
-              ),
-
-              Text(
-                'Repayment : RM $totalRepayment',
-              ),
-
-              Text(
-                'Eligibility : $loanApplicationResult',
               ),
 
               ElevatedButton(
-                  onPressed: _calculateRepayment,
-                  child: const Text('Calculate'),
-              )
+                  onPressed: _calculateWorkingHour,
+                  child: const Text('Calculate')
+              ),
+
+              Text(
+                'Your total paid is:  RM $totalPay',
+              ),
+
+              ElevatedButton(
+                  onPressed: _saveTotalPaid,
+                  child: const Text('Save your total paid')
+              ),
+
+              ElevatedButton(
+                  onPressed: _loadTotalPaid,
+                  child: const Text('Load your total paid')
+              ),
+
+              ElevatedButton(
+                  onPressed: _clearPaidList,
+                  child: const Text('Clear your total paid')
+              ),
             ],
           ),
         ),
@@ -221,3 +145,5 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
